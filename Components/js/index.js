fc1cee6aa0bf26aad0be4558afe3712ac04aa4e2
Aryan94581 <swamiaryan611@gmail.com>
2024-12-07 00:31:03 +0530

    update

--- Components/js/index.js
+++ Components/js/index.js
@@ -1,11 +1,70 @@
-new WOW().init();
-document.addEventListener("DOMContentLoaded", function () {
-    $(".header-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        nav: true,
-        dots: true,
+(function ($) {
+    "use strict";
+
+  
+    // wow js
+    new WOW().init();
+
+
+    // Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
     });
-});
+        
+    // Back to top btn
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // second  carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
 
